e5f44b73b4f950556ba915e22bf5a3df0fb3c7ab
sergei_darin <swd86@mail.ru>
2024-04-06 23:51:07 +0400

    Использование специального метода Hibernate.initialize()

--- src/main/java/org/example/AppForMovies.java
+++ src/main/java/org/example/AppForMovies.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.modelMovie.Director;
 import org.example.modelMovie.Movie;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -35,11 +36,26 @@ public class AppForMovies {
 //            createNewMovieForDirector(9,session);
 //            createNewDirectorWithNewMovies(session);
 
-//            showDirectorWithMoviesNew(1,session);
-            showMovieWithDirectorNew(1,session);
+            Director director = showDirectorWithMoviesNew(1,session);
+//            showMovieWithDirectorNew(1,session);
+            List<Movie>movies = director.getMovies();
 
-            //
+            //TODO: подгружаем ленивые сущности с помощью специального метода Hibernate
+            Hibernate.initialize(movies);
             session.getTransaction().commit();
+
+            // Если у нас загрузка Eager, то мы можем получить данные из связанной таблицы вне сессии
+            // т.к. у нас уже все подгрузилось в одном запросе
+
+            // При загрузке Lazy мы получим исключение org.hibernate.LazyInitializationException: failed to lazily initialize a collection
+            // Для того, чтобы можно было работать вне сессии достаточно вызвать метод sout(director.getMovies())
+            // внутри транзакции и затем уже вне ее можно вызывать тот же самый метод и ошибки не возникнте
+            /**
+             * если просто вызвать метод director.getMovies() без sout, то мы также получим ошибку
+             * так происходит потому что Java оптимизирует код и когда она видит, что объект никем не используется
+             * она может игнорировать его, в результате мы получим ошибку
+             */
+            System.out.println(movies);
         } finally {
             factory.close();
         }
@@ -116,17 +132,31 @@ public class AppForMovies {
     }
 
     // пример с загрузкой данных Lazy/Eager
-    public static void showDirectorWithMoviesNew(Integer directorId, Session session){
+
+    /**
+     * В данном случает связь между таблицами установлена как OneToMany
+     * от Director -> Movie и по умолчанию стоит ленивая загрузка, следовательно при получении
+     * директора из контекста, мы не получаем данные из связанной таблицы
+     *
+     * при вызове метода director.getMovies() идет еще один запрос к БД
+     */
+    public static Director showDirectorWithMoviesNew(Integer directorId, Session session){
         Director director = session.get(Director.class,directorId);
         System.out.println("Получение директора");
 
         // получение связанных сущностей лениво
-        System.out.println(director.getMovies());
-        System.out.println("Получение данных из связанной таблицы");
+//        System.out.println(director.getMovies());
+//        System.out.println("Получение данных из связанной таблицы");
+        return director;
     }
 
     // пример загрузки данных Lazy/Eager
-
+    /*
+    В данном примере у нас идет загрузка Eager по умолчанию, т.к. связь таблиц ManyToOne
+    от Movie -> Director. Поэтому у нас идет один запрос к БД при получении фильмов из контекста,
+    где уже загружены директоры. Соответственно при вызове метода movie.getDirector() у нас не
+    будет дополнительного запроса
+     */
     public static void showMovieWithDirectorNew(Integer movieId, Session session){
         Movie movie = session.get(Movie.class, movieId);
         System.out.println("Получение фильма");
